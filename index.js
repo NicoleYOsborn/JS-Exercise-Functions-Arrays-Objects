878c437f5c1bdba1ef0714bfc94581e76482eab7
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-04-16 16:44:26 -0700

    STRETCH getOlderCars passed!

--- index.js
+++ index.js
@@ -308,8 +308,16 @@ function getCarInfoById(inventory, number) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, maxYear) {
+  const oldCars = [];
+  for (let i=0; i<inventory.length; i++){
+    if(inventory[i].car_year <= maxYear){
+      oldCars.push(inventory[i]);
+      
+    }
+   
+  }
+  return oldCars;
 }
 
 /**
