0fa311b211c1850707dd1217745a8ee8d760c169
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-04-16 14:56:14 -0700

    challenge getName passes

--- index.js
+++ index.js
@@ -120,10 +120,12 @@ console.log(makePersonObject(5, "Leia", "leia@mail.com"));
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+function getName(obj) {
+      return `Hello, my name is ${obj.name}`;
+    }
+  
 
+console.log(getName({ id: 1, name: 'Leia', email: 'leia@leia.com'}));
 
 /**
  * ### Challenge `appleIndex`
